b4079b1c2915902082e5e5766031ab2973e08019
Soyon <samtaitai@gmail.com>
2022-06-05 21:35:05 -0400

    setting.py secret.json

--- NewsApp/settings.py
+++ NewsApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secret.json') # secrets.json 파일 위치를 명시
+secret_file = os.path.join(BASE_DIR, '/NewsApp/secret.json') # secrets.json 파일 위치를 명시
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
